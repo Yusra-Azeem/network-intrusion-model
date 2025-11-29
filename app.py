b31e986fe721c92ba86c6aa78e06e169a0cac211
Yusra Azeem <yuzhere55@gmail.com>
2025-11-29 16:33:11 +0530

    Update app.py
    cors update

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
 
 app = Flask(__name__, static_folder=FRONTEND_DIR)
-CORS(app)
+CORS(app, origins=["https://network-intrusion-model-9.onrender.com"])
 
 # ---------------------------
 #  LOAD MODEL + ENCODERS
